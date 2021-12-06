698b2e75f00d6184077075014522500c73289035
SrinidDev <45865576+SrinidDev@users.noreply.github.com>
2021-12-07 01:38:02 +0530

    Requirements

--- Stock.py
+++ Stock.py
@@ -48,7 +48,7 @@ def overview(start, end, df, mydict):
 
 def main():
 
-    df = pd.read_excel('ticker.xlsx')
+    df = pd.read_excel('https://github.com/SrinidDev/MINOR-PROJECT/blob/master/ticker.xlsx')
     st.header('Indian  Stock Market')
     st.subheader('The BSE and NSE')
     st.write("Most of the trading in the Indian stock market takes place on its two stock exchanges: "
